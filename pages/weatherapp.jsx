b1668774c7ae52ac141705c3ab4b56d6f69f5e14
Brian <31211719+brian4991@users.noreply.github.com>
2023-07-07 15:32:45 +0200

    fixed typo

--- pages/weatherapp.jsx
+++ pages/weatherapp.jsx
@@ -13,7 +13,7 @@ const weatherapp = () => {
           className='absolute z-1'
           layout='fill'
           objectFit='cover'
-          src={CarHubImg}
+          src={WeatherAppImg}
           alt='/'
         />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
